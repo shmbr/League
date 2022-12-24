d6b6a5ad4309bc74ec126dd955e8fad77d8e8ac2
yu_shmbr <ura.shambora@gmail.com>
2022-12-24 21:16:48 +0200

    Fix site layout and add cooperation section

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import Box from "@mui/material/Box";
 import { Paper } from "@mui/material";
-import Header from "./Components/App/Header"
-import TabsControl from "./Components/App/TabsControl"
-import Footer from "./Components/App/Footer"
-
-
+import Header from "./Components/App/Header";
+import TabsControl from "./Components/App/TabsControl";
+import Footer from "./Components/App/Footer";
 
 function App() {
   return (
-    <BrowserRouter width={800}>
-      <Box sx={{ pl: { xs: 0, sm: 5, md: 15 }, pr: { xs: 0, sm: 5, md: 15 } }}>
-        <Paper elevation={3}>
-          <Header />
-          <TabsControl />
-          <Footer />
-        </Paper>
-      </Box>
-    </BrowserRouter>
+    <Box
+      sx={{
+
+        display: {md: 'flex'},
+        alignItems: 'center',
+        justifyContent: 'center',
+
+      }}
+    >
+      <BrowserRouter>
+        <Box
+          sx={{
+
+
+            // pl: { xs: 0, sm: 5, md: 15 },
+            // pr: { xs: 0, sm: 5, md: 15 },
+            maxWidth: 900,
+          }}
+        >
+          <Paper elevation={3}>
+            <Header />
+            <TabsControl />
+            <Footer />
+          </Paper>
+        </Box>
+      </BrowserRouter>
+    </Box>
   );
 }
 export default App;
--- src/Components/Main/Cooperation.jsx
+++ src/Components/Main/Cooperation.jsx
@@ -1,31 +1,82 @@
-import { Typography, Paper,Box } from "@mui/material";
+import { Typography, Paper, Box, Button } from "@mui/material";
 import React from "react";
 //import { CardMedia } from "@mui/material";
+import Aeque from "../../Images/Main/Cooperation/aequo.jpg";
+import Avellum from "../../Images/Main/Cooperation/avellum.jpg";
+import Cms from "../../Images/Main/Cooperation/cms.jpg";
+import Eba from "../../Images/Main/Cooperation/eba.jpg";
+import Eterna from "../../Images/Main/Cooperation/eterna.jpg";
+import Ey from "../../Images/Main/Cooperation/ey.jpg";
+import Kpmg from "../../Images/Main/Cooperation/kpmg.jpg";
+import Lcf from "../../Images/Main/Cooperation/lcf.jpg";
 
-const list = ["123", "23", "34", "fmrel45", "fm3ekrp", "fmr5el", "fm9ekrp", "fm43rel", "fmek6rp", "fmek11rp", "fmrel", "fmekr1p", "fm232ekrp", "fmrel2312", "fm2ekrp"]
+import Banner1 from "../../Images/Main/home_banner_2.png";
+
+import { CardMedia, Grid } from "@mui/material";
+
+const list = ["Aeque", "Avellum", "Cms", "Eba", "Eterna", "Ey", "Kpmg", "Lcf"];
+const img = [Aeque, Avellum, Cms, Eba, Eterna, Ey, Kpmg, Lcf];
 
 const Cooperation = () => {
-
   return (
     <>
-      <Paper sx={{ background: "purple" }}>
-        <Typography textAlign={"center"}>Cooperation</Typography>
-      </Paper>
+      <Paper
+        sx={{
+          backgroundImage: `url(${Banner1})`,
+        }}
+      >
+        <Box sx={{ p: 3 }} textAlign={"center"}>
+          <Typography
+            variant={"h5"}
+            textAlign={"center"}
+            sx={{ p: 5, color: "white" }}
+          >
+            Ми співпрацюємо більш ніж з 30 великими компаніями
+          </Typography>
 
+          <Box
+            sx={{
+              display: "flex",
+              overflowX: "auto",
+              background: "white",
+            }}
+          >
+            {list.map((item) => (
+              <Box key={list.indexOf(item)}>
+                <Grid
+                  container
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  <Grid item sx={{ pr: 4, pl: 4, pt: 1, pb: 1 }}>
+                    <CardMedia
+                      component="img"
+                      image={img[list.indexOf(item)]}
+                      sx={{
+                        height: "auto",
+                        width: "auto",
+                        display: "flex",
+                        p: 1,
+                      }}
+                    />
+                  </Grid>
 
-      <div >
-        <Box sx={{
-          display: "flex",
-          overflowX: "auto",
-        }}>
-          {
-            list.map((item) => (
-              <Paper key={list.indexOf(item)} sx={{ p:4}}>{item}</Paper>
-            ))
-          }
+                  <Grid item>
+                    <Typography  sx={{ p: 1 }}>
+                      {item}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+            ))}
+          </Box>
+
+          <Button sx={{ color: "white", mt: 3 }}>Детальніше</Button>
         </Box>
-
-      </div>
+      </Paper>
     </>
   );
 };
